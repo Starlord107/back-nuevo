074e4c2ad749a6166427fd0617095353ca34770e
Starlord107 <alex7rodrivela10@gmail.com>
2025-12-04 19:20:13 +0100

    fix cambios en la base de datos

--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("./db");
-
+const db = require("../db");
 router.post("/", async (req, res) => {
     const { mesa_id, productos, total } = req.body;
 
